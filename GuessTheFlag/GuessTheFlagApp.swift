acf7f6969ce5b835e7865b7425518f613c283b6e
isiflo2728 <floresisidoro30@gmail.com>
2026-04-20 21:43:49 -0700

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Isidoro Flores on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GuessTheFlagTests/GuessTheFlagTests.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagTests.swift
+//  GuessTheFlagTests
+//
+//  Created by Isidoro Flores on 4/20/26.
+//
+
+import Testing
+@testable import GuessTheFlag
+
+struct GuessTheFlagTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
